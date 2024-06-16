eb4c39596bfc99edbf55fb3cf13e6cd8b49e877b
maria magdalena <neumariamagdalena@gmail.com>
2024-06-16 12:29:08 +0200

    trying to make game area appear instead of start screen and failing

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const startBtn = document.getElementById("start");
-const recordLogBtn = document.getElementById("record-log");
-const liveCount = document.getElementById("lives")
-const snakeChoiceBtn = document.getElementById("snakestyle");
-const lvlEasy = 
+document.addEventListener("DOMContentLoaded", () => {
+  const startScreen = document.getElementById("start-screen");
+  const gameScreen = document.getElementById("game-area");
+
+  const startBtn = document.getElementById("start");
+  const recordLogBtn = document.getElementById("record-log");
+  const liveCount = document.getElementById("lives");
+  const snakeChoiceBtn = document.getElementById("snakestyle");
+  const lvlEasy = document.getElementById("easy");
+  const lvlHard = document.getElementById("hard");
+
+  startBtn.addEventListener("click", () => {
+    startScreen.classList.add("hidden");
+    gameScreen.classList.remove("hidden");
+  });
+});
