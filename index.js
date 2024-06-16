f700c2e6fcb47563deb56e9b53d17a66a5716db3
maria magdalena <neumariamagdalena@gmail.com>
2024-06-16 12:03:22 +0200

    add consts for all areas on starting screen

---
+++ index.js
@@ -0,0 +1,5 @@
+const startBtn = document.getElementById("start");
+const recordLogBtn = document.getElementById("record-log");
+const liveCount = document.getElementById("lives")
+const snakeChoiceBtn = document.getElementById("snakestyle");
+const lvlEasy = 
