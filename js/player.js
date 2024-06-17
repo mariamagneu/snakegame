0eb0934f66900d57f1b223ec039d8ddc8c7ad552
maria magdalena <neumariamagdalena@gmail.com>
2024-06-17 12:40:29 +0200

    create move() that ensures player can move in all directions

--- js/player.js
+++ js/player.js
@@ -1,7 +1,31 @@
 class Player {
-  constructor(gameScreen) {
+  constructor(gameScreen, left, top, width, height, image) {
     this.gameScreen = gameScreen;
-    this.width = 40;
-    this.height = 80;
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.directionX = 0;
+    this.directionY = 0;
+    this.speed = 3;
+
+    this.element = document.createElement("img");
+    this.element.src = image;
+
+    this.element.style.position = "relative";
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+    this.element.style.width = `${width}px`;
+    this.element.style.height = `${height}px`;
+
+    this.gameScreen.appendChild(this.element);
+  }
+
+  move() {
+    this.left += this.directionX * this.speed;
+    this.top += this.directionY * this.speed;
+
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
   }
 }
